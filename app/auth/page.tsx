46ac9f0d3bc9ac8ce461796ee0ff818c93fae828
MarcoTheBigCreator <marco.rodriguez.is@unipolidgo.edu.mx>
2023-10-13 09:59:20 -0600

    add: login view

---
+++ app/auth/page.tsx
@@ -0,0 +1,45 @@
+import { Metadata } from "next"
+import Image from "next/image"
+import Link from "next/link"
+
+import { cn } from "@/lib/utils"
+import { buttonVariants } from "@/components/ui/button"
+import { UserAuthForm } from "@/app/auth/components/user-auth-form"
+
+export const metadata: Metadata = {
+  title: "Authentication",
+  description: "Authentication forms built using the components.",
+}
+
+export default function AuthenticationPage() {
+  return (
+    <>
+      <div className="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
+          <div className="absolute inset-0 img-bg" />
+          <div className="relative z-20 flex items-center text-lg font-medium">
+            Axbooks
+          </div>
+          <div className="relative z-20 mt-auto">
+            <blockquote className="space-y-2">
+              <p className="text-lg">
+                &ldquo;Become a part of a vibrant and growing community of book lovers.&rdquo;
+              </p>
+              <footer className="text-sm">Marco Rodríguez - CEO of Axbooks</footer>
+            </blockquote>
+          </div>
+        </div>
+        <div className="lg:p-8">
+          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+            <div className="flex flex-col space-y-2 text-center">
+              <h1 className="text-2xl font-semibold tracking-tight">
+                LOGIN
+              </h1>
+            </div>
+            <UserAuthForm />
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
