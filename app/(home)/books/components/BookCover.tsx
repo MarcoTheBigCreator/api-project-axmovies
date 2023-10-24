f33777b85d9050771f60b14415c8c2f66f36ea5c
MarcoTheBigCreator <marco.rodriguez.is@unipolidgo.edu.mx>
2023-10-24 01:30:59 -0600

    fix: API GET

--- app/(home)/books/components/BookCover.tsx
+++ app/(home)/books/components/BookCover.tsx
@@ -1,6 +1,9 @@
+'use client';
+import { useEffect, useState } from "react";
+
 import {
     CircleIcon,
-  } from "@radix-ui/react-icons"
+} from "@radix-ui/react-icons"
 
 
 import { Calendar } from "lucide-react"
@@ -12,35 +15,48 @@ import {
 
 
 export const BookCover = () => {
+
+    const [book, setBook] = useState([])
+
+    useEffect(() => {
+        fetch('/api/books')
+            .then(response => response.json())
+            .then(data => setBook(data));
+    }
+        , []);
+
+
     return (
         <>
             <div className="w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-0 md:gap-2 lg:gap-4 mt-4">
-                <div className="w-full space-y-3">
-                    <ContextMenu>
-                        <ContextMenuTrigger>
-                            <div className="overflow-hidden rounded-md">
-                                <img
-                                    src="https://upload.wikimedia.org/wikipedia/commons/1/1c/El_principito.jpg"
-                                    alt="librofeo"
-                                    className="h-[330px] w-[250px] transition-all hover:scale-105 aspect-[3/4]" />
+                {book.map((item) => (
+                    <div className="w-full space-y-3" key={item.id}>
+                        <ContextMenu>
+                            <ContextMenuTrigger>
+                                <div className="overflow-hidden rounded-md">
+                                    <img
+                                        src={item.link_image}
+                                        alt="librofeo"
+                                        className="h-[330px] w-[250px] transition-all hover:scale-105 aspect-[3/4]" />
+                                </div>
+                            </ContextMenuTrigger>
+                        </ContextMenu>
+                        <div className="space-y-1 text-sm">
+                            <h3 className="text-base font-semibold leading-none">{item.title}</h3>
+                            <p className="text-sm text-muted-foreground">{item.author}</p>
+                        </div>
+                        <div className="text-sm text-muted-foreground">
+                            <div className="flex items-center">
+                                <CircleIcon className="mr-1 h-3 w-3 fill-purple-700 text-purple-700" />
+                                {item.category}
                             </div>
-                        </ContextMenuTrigger>
-                    </ContextMenu>
-                    <div className="space-y-1 text-sm">
-                        <h3 className="text-base font-semibold leading-none">El principito</h3>
-                        <p className="text-sm text-muted-foreground">Thomas tu ptm</p>
-                    </div>
-                    <div className="text-sm text-muted-foreground">
-                        <div className="flex items-center">
-                            <CircleIcon className="mr-1 h-3 w-3 fill-purple-700 text-purple-700" />
-                            Terror
-                        </div>
-                        <div className="flex items-center">
-                            <Calendar className="mr-1 h-3 w-3 text-purple-700" />
-                            April 2023
+                            <div className="flex items-center">
+                                <Calendar className="mr-1 h-3 w-3 text-purple-700" />
+                                {item.year}
+                            </div>
                         </div>
                     </div>
-                </div>
+                    ))}
             </div>
         </>
     )
--- app/api/books/route.ts
+++ app/api/books/route.ts
@@ -1,16 +1,15 @@
 
 import { createClient } from "@supabase/supabase-js";
 
-export async function GET(request: Request) {
-  const supabase =  createClient(
-    process.env.SUPABASE_URL!, 
-    process.env.SUPABASE_KEY!
-    )
-    const { data, error } = await supabase
-    .from('books')
-    .select('*')
-    if (error) {
-      return new Response(error.message, { status: 500 })
-    }
-    return new Response(JSON.stringify(data), { status: 200 })
+export async function GET() {
+  try {
+    const supabase = createClient(process.env.SUPABASE_URL!,process.env.SUPABASE_KEY!);
+    const { data } = await supabase
+      .from("books")
+      .select()
+    return Response.json(data);
+  } catch (error) {
+    console.log(error);
+    return Response.json({ error: 'Something went wrong' }, { status: 500 })
+  }
 }
