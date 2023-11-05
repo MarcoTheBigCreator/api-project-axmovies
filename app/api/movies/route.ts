bb7d3ed61afc7ba3d8fa31aef023cf00b0ec4a66
MarcoTheBigCreator <marco.rodriguez.is@unipolidgo.edu.mx>
2023-11-04 18:42:31 -0600

    Add PUT route to update movie data.

--- app/api/movies/route.ts
+++ app/api/movies/route.ts
@@ -1,5 +1,5 @@
 
-import { MoviesForm } from "@/types/routes";
+import { MoviesForm, Movies } from "@/types/routes";
 import { createClient } from "@supabase/supabase-js";
 import { type NextRequest } from 'next/server';
 
@@ -44,3 +44,32 @@ export async function POST(request: NextRequest) {
   }
 
 }
+
+export async function PUT(request: NextRequest) {
+  
+    const dataIncoming: Movies = await request.json();
+  
+    if (!dataIncoming.Title || !dataIncoming.Director || !dataIncoming.Year || !dataIncoming.Category || !dataIncoming.Link_image) {
+      return Response.json({ error: 'Missing data' }, { status: 400 })
+    }
+  
+    try {
+      const supabase = createClient(process.env.SUPABASE_URL!, process.env.SUPABASE_KEY!);
+      const { data } = await supabase
+        .from('movies')
+        .update({ 
+          Title: `${dataIncoming.Title}`, 
+          Director: `${dataIncoming.Director}`, 
+          Year: `${dataIncoming.Year}`, 
+          Category: `${dataIncoming.Category}`, 
+          Link_image: `${dataIncoming.Link_image}` 
+        })
+        .match({ id: `${dataIncoming.id}` })
+        .select()
+      return Response.json(data);
+    } catch (error) {
+      console.log(error);
+      return Response.json({ error: 'Something went wrong' }, { status: 500 })
+    }
+  
+  }
